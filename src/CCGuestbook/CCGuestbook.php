a41ac3c414b0dd4f5b358c4fd1c6a135929ebb26
Andreas Carlsson <andreasc89@gmail.com>
2013-09-21 13:40:20 +0200

    Lade till ett interface IHasSQL och lade alla SQL-frågor i en egen statisk klass inuti controllern

--- src/CCGuestbook/CCGuestbook.php
+++ src/CCGuestbook/CCGuestbook.php
@@ -4,7 +4,7 @@
 * 
 * @package LatteCore
 */
-class CCGuestbook extends CObject implements IController {
+class CCGuestbook extends CObject implements IController, IHasSQL {
 
   private $pageTitle = 'Latte Guestbook Example';
   private $pageHeader = '<h1>Guestbook Example</h1><p>Showing off how to implement a guestbook in Latte.</p>';
@@ -18,16 +18,23 @@ class CCGuestbook extends CObject implements IController {
     parent::__construct();
   }
 
-  /**
-   * Create new database
-   */
-  private function CreateTableInDatabase() {
-    try {
-      $this->db->ExecuteQuery("CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));");
-    } catch(Exception$e) {
-      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
-    }
-  }
+   /**
+    * Implementing interface IHasSQL. Encapsulate all SQL used by this class.
+    *
+    * @param string $key the string that is the key of the wanted SQL-entry in the array.
+    */
+  public static function SQL($key=null) {
+     $queries = array(
+        'create table guestbook'  => "CREATE TABLE IF NOT EXISTS Guestbook (id INTEGER PRIMARY KEY, entry TEXT, created DATETIME default (datetime('now')));",
+        'insert into guestbook'   => 'INSERT INTO Guestbook (entry) VALUES (?);',
+        'select * from guestbook' => 'SELECT * FROM Guestbook ORDER BY id DESC;',
+        'delete from guestbook'   => 'DELETE FROM Guestbook;',
+     );
+     if(!isset($queries[$key])) {
+        throw new Exception("No such SQL query, key '$key' was not found.");
+      }
+      return $queries[$key];
+   }
   
 
   /**
@@ -57,6 +64,17 @@ class CCGuestbook extends CObject implements IController {
     }
   }
 
+  /**
+   * Create new database
+   */
+  private function CreateTableInDatabase() {
+    try {
+      $this->db->ExecuteQuery(self::SQL('create table guestbook'));
+    } catch(Exception$e) {
+      die("$e<br/>Failed to open database: " . $this->config['database'][0]['dsn']);
+    }
+  }
+
 
   /**
    * Handle posts from the form and take appropriate action.
@@ -78,7 +96,7 @@ class CCGuestbook extends CObject implements IController {
    * Save a new entry to database.
    */
   private function SaveNewToDatabase($entry) {
-    $this->db->ExecuteQuery('INSERT INTO Guestbook (entry) VALUES (?);', array($entry));
+    $this->db->ExecuteQuery(self::SQL('insert into guestbook'), array($entry));
     if($this->db->rowCount() != 1) {
       echo 'Failed to insert new guestbook item into database.';
     }
@@ -89,7 +107,7 @@ class CCGuestbook extends CObject implements IController {
    * Delete all entries from the database.
    */
   private function DeleteAllFromDatabase() {
-    $this->db->ExecuteQuery('DELETE FROM Guestbook;');
+    $this->db->ExecuteQuery(self::SQL('delete from guestbook'));
   }
 
   /**
@@ -98,7 +116,7 @@ class CCGuestbook extends CObject implements IController {
   private function ReadAllFromDatabase() {
     try {
       $this->db->SetAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-      return $this->db->ExecuteSelectQueryAndFetchAll('SELECT * FROM Guestbook ORDER BY id DESC;');
+      return $this->db->ExecuteSelectQueryAndFetchAll(self::SQL('select * from guestbook'));
     } catch(Exception $e) {
       return array();    
     }
