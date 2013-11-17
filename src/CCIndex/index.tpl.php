59b2122e0d3aae073fd8e098e0c1e5645d073ca3
Andreas Carlsson <andreasc89@gmail.com>
2013-11-17 10:02:20 +0100

    Lite nya formuleringar i instruktionerna

--- src/CCIndex/index.tpl.php
+++ src/CCIndex/index.tpl.php
@@ -12,13 +12,20 @@
 <p>You can review its source directly on github: <a href='https://github.com/andreasc89/latte'>https://github.com/andreasc89/latte</a></p>
 
 <h2>Installation</h2>
-<p>The installation process is collected in the <a href="<?=create_url('setup');?>">setup</a>-controller
+<p>The installation process can be found in the <a href="<?=create_url('setup');?>">setup</a>-controller</p>
 
 <? else: ?>
 
 
 <h2>Looks like it's your first time here.</h2>
 <p>Please go to the <a href="<?=create_url('setup');?>">setup</a>-controller to check that your server is capable of running the framework and connect the framework to the your MySQL-database.</p>
+
+<h2>Download</h2>
+<p>You can download Latte from github.</p>
+<blockquote>
+<code>git clone git://github.com/andreasc89/latte.git</code>
+</blockquote>
+<p>You can review its source directly on github: <a href='https://github.com/andreasc89/latte'>https://github.com/andreasc89/latte</a></p>
 
 
 
